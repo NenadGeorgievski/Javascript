e7db41a9e0df780edebdf747c25d1129115efa9f
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-07-02 00:55:21 +0200

    Practicing javascript

--- Practice/main.js
+++
@@ -1,5 +0,0 @@
-
-let miles = prompt("Enter distance in miles:");
-miles = Number(miles);
-let kilometars = miles / 1.60934;
-console.log( "The distance you entered in miles is:" + miles + "and in kilometars is" + kilometars.toFixed(2));
